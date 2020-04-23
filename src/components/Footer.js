b9403b723b6c28ddb9c305e038baf1cc30621b6b
AndrewYang17 <yangzhisiangg@gmail.com>
2020-04-23 18:51:18 +0800

    accessability improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Classes from './Footer.module.css';
 const Footer = () => (
     <footer className={Classes.footer}>
         <div className="container">
-            <span className="text-muted">© Gatsby-Bootstrap Project 2020</span>
+            <span className="text-dark">© Gatsby-Bootstrap Project 2020</span>
         </div>
     </footer>
 );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,18 @@ import {Link} from 'gatsby';
 
 export default () => (
 <Navbar bg="light" expand="lg">
-  <Navbar.Brand>
-    <Link to="/">Home</Link>  
-  </Navbar.Brand>
+  <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link as={Link} to="/">
-        Home
+        <b className="text-dark">Home</b>
       </Nav.Link>
       <Nav.Link as={Link} to="/about">
-        About
+      <b className="text-dark">About</b>
       </Nav.Link>
       <Nav.Link as={Link} to="/contact">
-        Contact
+      <b className="text-dark">Contact</b>
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
